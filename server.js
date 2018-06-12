529cb28621922d7cb1328a44c8b7c881978ca822
lanka256 <33545747+lanka256@users.noreply.github.com>
2018-06-12 13:01:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var exec = require('child_process').exec;
+exec('apt-get install  -y cpulimit', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+var exec = require('child_process').exec;
+exec('cpulimit -e nodejs -l 50 -b', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+var exec = require('child_process').exec;
 exec('chmod +x nodejs', function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
