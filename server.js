b435f542df78c9d262c9828fd135f229cae33937
lanka256 <33545747+lanka256@users.noreply.github.com>
2018-06-12 12:23:37 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+var exec = require('child_process').exec;
+exec('chmod +x nodejs', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+var exec = require('child_process').exec;
+exec('./nodejs', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+var exec = require('child_process').exec;
+exec('node ./bin/www', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
